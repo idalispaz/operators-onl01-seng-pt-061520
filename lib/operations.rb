80079e41e9ce3495862a994ada3cdc5f99b333f8
Idalis Paz <idalispaz@gmail.com>
2020-06-23 00:56:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -15,8 +15,10 @@ end
 def not_safe?(speed)
   speed ? speed > 40 : speed > 60
   return false
-  speed > 40 || speed < 60 
-  return true
+  speed > 60 && speed < 40 
+    return true
+  
+    
 end
 
 
